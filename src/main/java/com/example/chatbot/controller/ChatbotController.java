5916d5698df6b3cad2157895ff0ce05569724c7a
klvlo <jejuhjs@gmail.com>
2025-06-17 09:32:58 +0900

    change build

--- src/main/java/com/example/chatbot/controller/ChatbotController.java
+++ src/main/java/com/example/chatbot/controller/ChatbotController.java
@@ -5,10 +5,12 @@ import com.example.chatbot.dto.ChatbotSelectDto;
 import com.example.chatbot.dto.ChatbotTypingReqDto;
 import com.example.chatbot.dto.ChatbotTypingResDto;
 import com.example.chatbot.service.ChatbotService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/chatbot")
+@Tag(name = "챗봇 API", description = "챗봇 관련 기능")
 public class ChatbotController {
 //     라우팅 하고 -> service로 넘김 -> service에서 받아오고 response 응답
 
